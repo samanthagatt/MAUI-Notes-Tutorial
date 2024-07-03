36afa18fa4d2949a982869e8f6dcc8fee8e5893a
Samantha Gatt <smfgatt@gmail.com>
2024-07-02 22:11:38 -0400

    Initial commit

---
+++ MauiNotesTutorial/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiNotesTutorial;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiNotesTutorial/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiNotesTutorial;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
