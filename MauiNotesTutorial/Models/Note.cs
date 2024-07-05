59a327a3126c2e779b3ac7edfb6217adf8b59078
Samantha Gatt <smfgatt@gmail.com>
2024-07-04 21:36:49 -0400

    Refactors to use MVVM

--- MauiNotesTutorial/Models/Note.cs
+++ MauiNotesTutorial/Models/Note.cs
@@ -5,4 +5,43 @@ public class Note
     public string Filename { get; set; }
     public string Text { get; set; }
     public DateTime Date { get; set; }
+
+    public Note()
+    {
+        Filename = $"{Path.GetRandomFileName()}.notes.txt";
+        Date = DateTime.Now;
+        Text = "";
+    }
+
+    public void Save() =>
+        File.WriteAllText(Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+    public void Delete() =>
+        File.Delete(Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+    public static Note Load(string filename)
+    {
+        filename = Path.Combine(FileSystem.AppDataDirectory, filename);
+
+        if (!File.Exists(filename))
+            throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+        return new()
+        {
+            Filename = Path.GetFileName(filename),
+            Text = File.ReadAllText(filename),
+            Date = File.GetLastWriteTime(filename)
+        };
+    }
+
+    public static IEnumerable<Note> LoadAll()
+    {
+        // Use Linq extensions to load the *.notes.txt files.
+        return Directory
+                // Get all files that end in ".notes.txt"
+                .EnumerateFiles(FileSystem.AppDataDirectory, "*.notes.txt")
+                // Equivalent to map
+                .Select(filename => Note.Load(Path.GetFileName(filename)))
+                .OrderByDescending(note => note.Date);
+    }
 }
---
+++ MauiNotesTutorial/ViewModels/NotesViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiNotesTutorial.ViewModels;
+
+public class NotesViewModel : IQueryAttributable
+{
+    public ObservableCollection<NoteViewModel> AllNotes { get; }
+    public ICommand NewCommand { get; }
+    public ICommand SelectNoteCommand { get; }
+
+    public NotesViewModel()
+    {
+        AllNotes = new ObservableCollection<NoteViewModel>(Models.Note.LoadAll().Select(n => new NoteViewModel(n)));
+        NewCommand = new AsyncRelayCommand(NewNoteAsync);
+        SelectNoteCommand = new AsyncRelayCommand<NoteViewModel>(SelectNoteAsync);
+    }
+
+    private async Task NewNoteAsync()
+    {
+        await Shell.Current.GoToAsync(nameof(Views.NotePage));
+    }
+
+    private async Task SelectNoteAsync(NoteViewModel note)
+    {
+        if (note != null)
+            await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+    }
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("deleted"))
+        {
+            string noteId = query["deleted"].ToString();
+            NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            if (matchedNote != null)
+                AllNotes.Remove(matchedNote);
+        }
+        else if (query.ContainsKey("saved"))
+        {
+            string noteId = query["saved"].ToString();
+            NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            if (matchedNote != null)
+                matchedNote.Reload();
+            else
+                AllNotes.Add(new NoteViewModel(Models.Note.Load(noteId)));
+        }
+    }
+}
